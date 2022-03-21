a18a2612ace5c9e2421b040b91aaed46332c56b3
Ausland3r <Ausland3r@yandex.ru>
2022-03-21 13:09:37 +0300

    test

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from OperateRobot import OperateRobot
 from OperateCamera import OperateCamera
+import time
 
 
+rob = OperateRobot("172.31.1.25")
+pos = rob.getl()
+pos[0] += 0.1
+
+moving_coordinates = {"x": pos[0], "y": pos[1], "z": pos[2], "rx": pos[3], "ry": pos[4], "rz": pos[5]}
+
+rob.movel(moving_coordinates)
+rob.open_gripper()
+time.sleep(5)
+rob.close_gripper();
+time.sleep(2)
+rob.close()
